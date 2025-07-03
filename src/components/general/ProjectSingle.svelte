6951eae379dda8278c9b6676029968a36826cd0f
orlando-helfer <orlandohrabaca@gmail.com>
2025-07-03 21:58:26 +0200

    add merkeliste save-button

--- src/components/general/ProjectSingle.svelte
+++ src/components/general/ProjectSingle.svelte
@@ -113,7 +113,26 @@
 		</div>
 
 		<div class="description-container">
-			<h1 class="title">{getLocalizedLabel(project.title, $activeLanguage)}</h1>
+			<div class="description-title-container">
+				<h1 class="title">{getLocalizedLabel(project.title, $activeLanguage)}</h1>
+				<button
+					class="save-button"
+					class:saved={isSaved}
+					on:click={handleSaveClick}
+					aria-label={isSaved
+						? getUIText('merkliste.removeFromMerkliste', $activeLanguage)
+						: getUIText('merkliste.addToMerkliste', $activeLanguage)}
+					data-tooltip={isSaved
+						? getUIText('merkliste.removeFromMerkliste', $activeLanguage)
+						: getUIText('merkliste.addToMerkliste', $activeLanguage)}
+				>
+					{#if isSaved}
+						<img src="{base}/icons/basket_full.png" alt="Remove" class="basket-icon" />
+					{:else}
+						<img src="{base}/icons/basket_empty.png" alt="Add" class="basket-icon" />
+					{/if}
+				</button>
+			</div>
 			<p class="author">{project.author}</p>
 			{#if project.coauthors && project.coauthors.length > 0}
 				<div class="coauthors">
@@ -122,7 +141,7 @@
 					{/each}
 				</div>
 			{/if}
-			<br />
+			<!-- <br /> -->
 			<div class="location-format-section">
 				<p class="category">
 					{project.formats.map((format) => getLocalizedLabel(format, $activeLanguage)).join(', ')}
@@ -214,6 +233,12 @@
 			grid-row: 3 / 4;
 			transform: rotate(0deg);
 		}
+	}
+
+	.description-title-container {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
 	}
 
 	.title-image-container {
@@ -319,8 +344,7 @@
 		transition: all 200ms ease-in-out;
 		z-index: 20;
 		display: flex;
-		align-items: center;
-		justify-content: center;
+		align-self: flex-start;
 		gap: 0.5rem;
 		white-space: nowrap;
 		flex-shrink: 0;
